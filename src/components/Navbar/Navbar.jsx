822618186db213af908e130ce5deecc5197b288d
Hugo-rvs31 <hug.rvs31@gmail.com>
2026-08-03 18:24:08 +0200

    modification Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,27 @@
+import { Link } from "react-router-dom";
 import "./Navbar.scss";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <nav>
+      <Link to="/">
+        <h2>TITAN GYM</h2>
+      </Link>
+
+      <ul>
+        <li>À propos</li>
+        <li>Tarifs</li>
+        <li>Coachs</li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+
+      <Link to="/contact" className="cta-button">
+        Essai gratuit
+      </Link>
+    </nav>
+  );
 };
 
 export default Navbar;

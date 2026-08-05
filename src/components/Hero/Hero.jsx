e8971fca2fa77ec53c6d576d6ae10c242230008e
Hugo-rvs31 <hug.rvs31@gmail.com>
2026-08-05 18:57:03 +0200

    modification 3

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,5 @@
 import "./Hero.scss";
+import { Link } from "react-router-dom";
 
 import heroImage from "../../assets/images/hero.jpg";
 
@@ -11,11 +12,17 @@ const Hero = () => {
       }}
     >
       <div className="hero-content">
-        <h1>DEVENEZ PLUS FORT QUE VOUS HIER</h1>
-
-        <p>Le meilleur accompagnement pour atteindre vos objectifs sportifs.</p>
-
-        <button>Essai gratuit</button>
+        <div className="hero-text">
+          <h1>DEVENEZ PLUS FORT QUE VOUS HIER</h1>
+          <p>
+            Le meilleur accompagnement pour atteindre vos objectifs sportifs.
+          </p>
+        </div>
+        <div className="hero-actions">
+          <Link to="/contact" className="cta-button">
+            Essai gratuit
+          </Link>
+        </div>
       </div>
     </section>
   );
